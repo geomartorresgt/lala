b9440fe6c755c34f14c20788ce0b2cba272f9953
Rodrigo <contacto@rodrigomedeiro.com>
2019-04-18 16:13:05 -0300

    modificacion muebles

--- public/editor/js/items.js
+++ public/editor/js/items.js
@@ -4,47 +4,82 @@ $(document).ready(function() {
   var items = [
  
     {
-      "name" : "3",
+      "name" : "AÉREO 0.40 1 PTA. A",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
-      "model" : "models/js/3/3.js",
-      "type" : "1"
+      "model" : "models/js/3a/3.js",
+      "type" : "2",
+      "cat" : "anza"
+    },
+    {
+      "name" : "AÉREO 0.30 1 PTA. B",
+      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+      "model" : "models/js/3b/3.js",
+      "type" : "2",
+      "cat" : "anza"
+    },  
+    {
+      "name" : "AÉREO BODEGA 0.20BAA/30",
+      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+      "model" : "models/js/4a/4.js",
+      "type" : "2",
+      "cat" : "anza"
     }, 
     {
-      "name" : "4",
+      "name" : "AÉREO BODEGA 0.15BAA826",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
-      "model" : "models/js/4/4.js",
-      "type" : "1"
+      "model" : "models/js/4b/4.js",
+      "type" : "2",
+      "cat" : "anza"
     }, 
     {
-      "name" : "5",
+      "name" : "MUEBLE PANELEIRO",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
       "model" : "models/js/5/5.js",
-      "type" : "1"
+      "type" : "2",
+       "cat" : "anza"
     }, 
     {
-      "name" : "7",
+      "name" : "AÉREO MICRO GRANDE",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
       "model" : "models/js/7/7.js",
-      "type" : "1"
+      "type" : "2",
+      "cat" : "anza"
     }, 
     {
-      "name" : "8",
+      "name" : "AÉREO 0.80 PTA. REBATIBLE VIDRIO",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
       "model" : "models/js/8/8.js",
-      "type" : "1"
+      "type" : "2",
+      "cat" : "anza"
     }, 
     {
-      "name" : "9",
+      "name" : "AÉREO 0.80 PTRA. REBATIBLE VIDRIO",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
       "model" : "models/js/9/9.js",
-      "type" : "1"
+      "type" : "2",
+      "cat" : "anza"
+    }, 
+    {
+      "name" : "AÉREO TERMINAL",
+      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+      "model" : "models/js/10/10.js",
+      "type" : "2",
+      "cat" : "anza"
+    }, 
+    {
+      "name" : "MUEBLE AÉREO 1.20 3 PTAS.",
+      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+      "model" : "models/js/11/11.js",
+      "type" : "2",
+      "cat" : "anza"
     }, 
    /*     
    {
       "name" : "",
       "image" : "",
       "model" : "",
-      "type" : "1"
+      "type" : "1",
+      "cat" : ""
     }, 
     */
   ]
@@ -56,7 +91,7 @@ $(document).ready(function() {
   for (var i = 0; i < items.length; i++) 
   {
 	var item = items[i];
-    itemsDiv = $("#"+modelTypesIds[modelTypesNum.indexOf(item.type)]+"-wrapper");
+    itemsDiv = $("#"+item.cat);
 	var modelformat = (item.format) ?' model-format="'+item.format+'"' : "";
     var html = '<div class="col-sm-4">' + '<a class="thumbnail add-item"' +' model-name="'+ item.name +'"' +' model-url="' +item.model+'"' +' model-type="' +item.type+'"' + modelformat+'>'+'<img src="'+item.image +'" alt="Add Item"  data-dismiss="modal"> '+item.name +'</a></div>';
     itemsDiv.append(html);
