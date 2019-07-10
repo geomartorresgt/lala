cc584e80e647bab3d36be54402f7ec0a197b3f36
Pedro Perez <pjperez.7490@gmail.com>
2019-07-10 11:41:04 -0400

    Limpiando el código

--- resources/views/admin/config/datos-masivos/js/index.blade.php
+++ resources/views/admin/config/datos-masivos/js/index.blade.php
@@ -14,8 +14,6 @@
             var ruta = '';
             var token = $('meta[name=csrf-token]').attr('content');
 
-
-
             if (valorLocal == '') {
                 alert('Debe seleccionar un local');
                 return;
@@ -28,7 +26,6 @@
 
             if (valorAccion == '1' && $archivo[0].files.length === 0 ) {
                 alert('Debe cargar un archivo')
-                // .get(0).files.length
                 return;
             }
 
@@ -45,38 +42,6 @@
             $form.submit();
 
             return;
-
-            
-
-            $.ajax({
-                url: ruta,
-                type: 'POST',
-                headers: {'X-CSRF-TOKEN': token},
-                // datatype: 'json',
-                cache: false,
-                beforeSend: function () {
-                    // $("#presupuesto_id option").hide();
-                    // $("#presupuesto_id option[data-retenedor='" + idRetenedor + "']").fadeIn();
-                    // $('#presupuesto_id option:first-child').attr('selected', 'selected');
-                },
-                success: function (respuesta) {
-                    console.log('todo bien');
-                    if (respuesta.success) {
-                        
-                    }
-
-                    // toastr.error(respuesta.error);
-                },
-                error: function (e) {
-                    console.log(e);
-                    // $.each(e.responseJSON.errors, function (index, element) {
-                    //     if ($.isArray(element)) {
-                    //         toastr.error(element[0]);
-                    //     }
-                    // });
-                }
-            });
-
         })
 
         $('#accion').change(function(e) {
