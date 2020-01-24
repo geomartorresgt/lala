a20274fb72539fb272d2ad513fa1e99f690a9bb8
Jesús Torres <jesustc17@gmail.com>
2020-01-24 15:44:51 -0400

    adaptaciones

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@
 Auth::routes();
 
 Route::group(['middleware' => ['auth', "session_time"]], function (){
-	Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+	Route::group(['prefix' => 'Admin', 'namespace' => 'Admin'], function () {
 		Route::name('root_path')->get('/', 'InicioController@index');
 		
 		Route::resource('/preguntas-frecuentes', 'PreguntasFrecuentesController', ['parameters' => [
@@ -45,7 +45,7 @@ Route::group(['middleware' => ['auth', "session_time"]], function (){
 	});
 });
 
-Route::group(['namespace' => 'web'], function () {
+Route::group(['namespace' => 'Web'], function () {
 	Route::get('/', 'InicioController@home')->name('root_path');
 	Route::get('/preguntas-frecuentes', 'PreguntasFrecuentesController@index')->name('web.preguntasFrecuentes.index');
 	Route::get('/eventos', 'EventosController@index')->name('web.eventos.index');
