6c090d47eb775ba7dd2bd70d3973d5a20f1063ec
Jesús Torres <jesustc17@gmail.com>
2020-01-25 18:43:55 -0400

    corección title en layout admin

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>..:: Funcación LALA ::..</title>
+    <title>..:: Fundación LALA ::..</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="https://unpkg.com/@coreui/icons/css/coreui-icons.min.css">
--- resources/views/web/partials/_header.blade.php
+++ resources/views/web/partials/_header.blade.php
@@ -1,3 +1,21 @@
+<style>
+    video::-webkit-media-controls {
+        /* display: none; */
+        visibility: hidden;
+    }
+
+    /* Could Use thise as well for Individual Controls */
+    video::-webkit-media-controls-play-button {}
+
+    video::-webkit-media-controls-volume-slider {}
+
+    video::-webkit-media-controls-mute-button {}
+
+    video::-webkit-media-controls-timeline {}
+
+    video::-webkit-media-controls-current-time-display {}
+</style>
+
 <header style="background-color: #000" >
-    <video id="video_banner" src="video/video.mp4" autoplay="true" loop  ></video>
+    <video id="video_banner" controls src="video/video.mp4" autoplay="true" loop  ></video>
 </header>
